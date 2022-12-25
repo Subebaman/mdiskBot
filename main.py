9c6b7ffd3084463d2ca5adb484aa242e4cc49b6c
Subebaman <106474658+Subebaman@users.noreply.github.com>
2022-12-25 12:37:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ async def message_handler(event):
         message = f'**Click Here 👇 For "{event.text}"**\n\n[🍿🎬 {str(event.text).upper()}\n🍿🎬 {str("Click me for results").upper()}]({tgraph_result})'
 
         newbutton = [Button.url('How To Watch ❓',
-                                    f'https://t.me/postsearchbot?start=Watch')]
+                                    f'https://t.me/howto_download_movies_hg')]
 
         await txt.delete()
         await asyncio.sleep(0.5)
